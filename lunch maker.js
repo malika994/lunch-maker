d9825b53514dff103e8f06dacba388613099422f
malika <idrissirat8@gmail.com>
2025-02-19 00:15:33 +0300

    luncher maker functions are added

---
+++ lunch maker.js
@@ -0,0 +1,71 @@
+let lunches = [];
+function addLunchToEnd(lunches, lunchItem) {
+    lunches.push(lunchItem);
+    console.log(`${lunchItem} added to the end of the lunch menu.`);
+    return lunches;
+}
+addLunchToEnd(lunches, ["Pizza", "Tacos", "Burger"]);
+
+//Add lunch to start  function
+function addLunchToStart(lunches, lunchItem) {
+    lunches.unshift(lunchItem);
+    console.log(`${lunchItem} added to the start of the lunch menu.`);
+    return lunches;
+}
+addLunchToStart(lunches, ["Burger", "Sushi", "Pizza"]);
+
+//Remove last element of lunches array
+function removeLastLunch(lunches) {
+
+    if (lunches.length !== 0) {
+        let popedLunch = lunches.pop();
+        console.log(`${popedLunch} removed from the end of the lunch menu.`);
+    }
+    else {
+        console.log("No lunches to remove.");
+    }
+    return lunches;
+}
+removeLastLunch(["Stew", "Soup", "Toast"]);
+
+//Remove first element of linches array
+function removeFirstLunch(lunches) {
+    if (lunches.length != 0) {
+        let removedItem = lunches.shift();
+        console.log(`${removedItem} removed from the start of the lunch menu.`)
+
+    }
+    else {
+        console.log("No lunches to remove.");
+    }
+    return lunches;
+}
+removeFirstLunch(["Salad", "Eggs", "Cheese"]);
+
+//get Random Lunch function:
+function getRandomLunch(lunches) {
+    let arrLength = lunches.length;
+    if (lunches.length == 0) {
+        console.log('No lunches available.');
+    }
+    else {
+        let randNumber = Math.floor(Math.random() * arrLength);
+        let randElement = lunches[randNumber];
+        console.log(`Randomly selected lunch: ${randElement}`);
+    }
+}
+getRandomLunch(["Salad", "Eggs", "Cheese"]);
+
+
+// funation show Lunch Menu
+function showLunchMenu(lunches) {
+
+    if (lunches.length != 0) {
+        console.log(`Menu items:${lunches}`);
+    }
+    else {
+        console.log('The menu is empty.');
+    }
+}
+//showLunchMenu([]);
+showLunchMenu(["Greens", "Corns", "Beans"]);
